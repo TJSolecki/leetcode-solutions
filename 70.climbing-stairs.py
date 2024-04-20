5f3be6712e19a7e33c6f42b25a61078faa2b6001
Thomas Solecki <thomasjsolecki@gmail.com>
2024-04-20 10:53:38 -0700

    Add leetcode #70 Climbling Stairs solution

---
+++ 70.climbing-stairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        stairs = {1: 1, 2: 2}
+        for i in range(3, n + 1):
+            stairs[i] = stairs[i - 1] + stairs[i - 2]
+
+        return stairs[n]
